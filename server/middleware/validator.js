b3a6b70e4fc4047470c27830d02ff4902a8b1da6
Zjion <chngguohao@hotmail.com>
2016-01-23 17:27:46 +0800

    add some functions for validator

--- server/middleware/validator.js
+++ server/middleware/validator.js
@@ -1,3 +1,5 @@
+lodash = require('lodash');
+
 module.exports = {
   /**
    * Check if fulfill model for linegraph
@@ -5,6 +7,27 @@ module.exports = {
    * @return {Boolean}     [description]
    */
   isLineGraph: function(arg){
+  	
+  	
+  	for (var i=0;i<arg.length;i++) {
+  		//check for color ,if no exist, dafault String
+	  	var isColor  = /^#[0-9A-F]{6}$/i.test(arg[i].color);
+
+	  	if (isColor == false) {
+	  		arg[i].color = "FFFFFF"
+	  	}
+	  	//check if nodes is array of nodes of 2 elements??
+
+	  	//check if x is number
+	  	if (!_.isNumber(arg[i].nodes[0].x)) {
+	  		return "err x";
+	  	}
+	  	//check if y is number
+	  	if (!_.isNumber(arg[i].nodes[0].y)) {
+	  		return "err y";
+	  	}
+
+  	}
 
   }
 
