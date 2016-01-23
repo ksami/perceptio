dc2d0d23ffcfd50efd9645832efc36dee91f91df
thearith <sothearith.sreang@gmail.com>
2016-01-23 21:32:42 +0800

    fix index page

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,26 +1,13 @@
 $(function() {
 
+  $('#scrolling a').bind('click',function(event){
+    var $anchor = $(this);
 
-  function filterPath(string) {
-    return string
-      .replace(/^\//,'')
-      .replace(/(index|default).[a-zA-Z]{3,4}$/,'')
-      .replace(/\/$/,'');
-  }
+    $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top
+    }, 1000,'easeInOutExpo');
 
-  $('a[href*=#]').each(function() {
-    if ( filterPath(location.pathname) == filterPath(this.pathname)
-    && location.hostname == this.hostname
-    && this.hash.replace(/#/,'') ) {
-      var $targetId = $(this.hash), $targetAnchor = $('[name=' + this.hash.slice(1) +']');
-      var $target = $targetId.length ? $targetId : $targetAnchor.length ? $targetAnchor : false;
-       if ($target) {
-         var targetOffset = $target.offset().top;
-         $(this).click(function() {
-           $('html, body').animate({scrollTop: targetOffset}, 400);
-           return false;
-         });
-      }
-    }
+    event.preventDefault();
   });
+
 });
