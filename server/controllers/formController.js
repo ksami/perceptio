cc4500f7fe2d633ece4d439c9115b6326c50d5e7
ksami <ksami.ihide@gmail.com>
2016-01-23 17:43:17 +0800

    validator test

--- server/controllers/formController.js
+++ server/controllers/formController.js
@@ -1,16 +1,25 @@
 var Models = require('../models');
 var respond = require('./apiResponse');
+var Validator = require('../middleware/validator');
 
 var FormController = {
 
   // POST /form
   create: (req, res, next)=>{
-    var lineGraph =  new Models.LineGraph(req.body);
-    lineGraph.save()
-    .then(function(model){
-      console.log(model);
-      res.send(respond(lineGraph));
-    });
+    var valid = Validator.isLineGraph(req.body);
+    
+    if(!valid.result){
+      res.send(respond(null, valid.error));
+    }
+    else{
+      var lineGraph =  new Models.LineGraph(req.body);
+      lineGraph.save()
+      .then(function(model){
+        console.log(model);
+        res.send(respond(lineGraph));
+      });
+    }
+    
     next();
   }
 
